8969713fe3e0210c2807e72e602e1ede4eb07d19
kanhu <testkanhucharan@gmail.com>
2021-05-04 11:05:56 +0530

    code change

--- src/helloworld/Hwllo.java
+++ src/helloworld/Hwllo.java
@@ -3,7 +3,7 @@ package helloworld;
 public class Hwllo {
 
 	public static void main(String[] args) {
-		System.out.print(Integer.parseInt("-100"));
+		System.out.print(Integer.parseInt("100"));
 
 	}
 
